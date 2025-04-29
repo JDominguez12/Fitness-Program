fcd3a4f77e5ffca852d43d141c2970fa5f04fc2d
JDominguez12 <jadominguez170@gmail.com>
2025-04-28 17:58:06 -0700

    added line 4.

--- fitnessProgram.java
+++ fitnessProgram.java
@@ -1,5 +1,7 @@
 public class fitnessProgram {
     public static void main(String[] args) {
         System.out.println("Welcome to the Fitness Program!");
+        System.out.println("This is the current change.");
+
     }
 }
