793336b4bb6d89070546f390875880746a49abe4
JDominguez12 <144716213+JDominguez12@users.noreply.github.com>
2025-05-11 15:26:08 -0700

    modified fitnessProgram.Java and added bmi.java

---
+++ bmi.java
@@ -0,0 +1,43 @@
+import java.util.InputMismatchException;
+public class bmi {
+	private int weight;
+    private int height;
+    public void setWeight(int weight){
+        this.weight = weight;
+    }
+    public int getWeight(){
+        return weight;
+    }
+    public  void setHeight(int height){
+        this.height = height;
+    }
+    public int getHeight(){
+        return height;
+    }
+    public double getBMI(int weight, int height) throws Exception{
+        double bmiCalc = 0.0;
+        try{
+            if (weight < 0 || height < 0){
+                throw new Exception("Error: weight and height must be positive numbers");
+            }
+            else if (weight == 0 || height == 0){
+                throw new ArithmeticException("Error: height cannot be zero");
+            }
+            bmiCalc = ((double) weight / (height * height)) * 703;
+            System.out.print("Your BMI is: ");
+            System.out.printf("%.2f", bmiCalc);
+            
+        }
+        catch(InputMismatchException e){
+            System.out.println("Error: Please enter a number");
+        }
+        catch (ArithmeticException a){
+            System.out.println(a.getMessage());
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        return bmiCalc;
+    }
+
+}
