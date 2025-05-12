edc7fda08ac863529d4b116feee2c9ae1861a5c0
yaecob14 <jacob06humphrey@gmail.com>
2025-05-11 19:36:46 -0700

    min menu
    main menu

--- fitnessProgram.java
+++ fitnessProgram.java
@@ -1,13 +1,21 @@
 public class fitnessProgram {
     public static void main(String[] args) {
+        canner scnr = new Scanner(System.in);
+        MenuHandler menuHandler = new MenuHandler();
+
         System.out.println("Welcome to the Fitness Program!");
-        System.out.println("This is the current change.");
-        //change2
-        //change3
-        //change4
-        //change5
-        // change6
-        //change7
-        //Java Sucks
+
+        while (true) {
+            try {
+                menuHandler.displayMenu();
+                System.out.print("Enter option: ");
+                int choice = scnr.nextInt();
+                menuHandler.handleSelection(choice, scnr);
+            } catch (Exception e) {
+                System.out.println("Invalid input. Please enter a number between 1 and 3.");
+                scnr.nextLine(); // Clear buffer
+            }
+        }
     }
 }
+
