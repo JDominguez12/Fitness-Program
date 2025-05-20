0da83c229f8d2126edbacb3b31c11c7c3f2d4366
JDominguez12 <144716213+JDominguez12@users.noreply.github.com>
2025-05-19 18:23:47 -0700

    Update fitnessProgram.java

--- fitnessProgram.java
+++ fitnessProgram.java
@@ -2,36 +2,37 @@ import java.util.Scanner;
 import java.util.InputMismatchException;
 
 public class fitnessProgram {
-	public static void SetupBMI(Scanner scnr) {
-		bmi user1 = new bmi();
+	public static void BMICalculator(Scanner scnr) {
+	bmi user1 = new bmi();
         int weight = 0;
         int height = 0;
-        System.out.println(" ");
        
         while(true){
            try{
-                System.out.println("Enter weight: ");
+                System.out.println("Enter weight in pounds: ");
                 weight = scnr.nextInt();
+		System.out.println("Enter your height in inches: );
                 height= scnr.nextInt();
                 break;
             } catch (InputMismatchException e) {
                 System.out.println("Error: Please enter a number.");
                 scnr.next();
-           }
-        }
+             }
+          }
         
-        user1.setWeight(weight);
-        user1.setHeight(height);
-        try {
-            user1.getBMI(weight, height);
-        } catch (Exception e) {
+          user1.setWeight(weight);
+          user1.setHeight(height);
+          try {
+              user1.getBMI(weight, height);
+          } catch (Exception e) {
             System.out.println(e.getMessage());
-        }
-	}
+          }
+    }
     public static void main(String[] args) {
     	Scanner scnr = new Scanner(System.in);
-        System.out.println("Welcome to the Fitness Program!");
-        System.out.println("This is the current change.");
+       
+	menu(scnr);
+	scnr.close();
 
     }
 }
