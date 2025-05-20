1e9f272cfb920394910e94d090d5d36670c8d0a0
yaecob14 <jacob06humphrey@gmail.com>
2025-05-19 19:50:21 -0700

    Update fitnessProgram.java
    added more code

--- fitnessProgram.java
+++ fitnessProgram.java
@@ -18,4 +18,53 @@ public class fitnessProgram {
         }
     }
 }
+  public static void menu(Scanner scnr) {
+      int choice = 0
+
+      while (true) {
+          System.out.println(x:"1. BMI Calculator");
+          System.out.println(x:"2. Workout Plan");
+          System.out.println(x:"3. Exit");
+          System.out.println(s:"Enter your choice: ");
+
+          try {
+              choice = scnr.nextInt();
+              if (choice == 4) {
+                  System.out.println(x:"Exiting...");
+                  break;
+              }
+              menuSelect(choice, scnr);
+          } catch(InputMismatchExeption e) {
+              System.out.println(x:"Invalid input. Please enter a number.");
+          }
+      }
+}
+  public static void menuSelect(int choice, Scanner scnr) {
+      switch (choice) {
+          case 1:
+              BMICalculator(scnr);
+              break;
+          case 2:
+              System.out.println(x:"Workout Plan");
+              new workoutPlan();
+              break;
+          case 3:
+              System.out.println(x:"Weekly Report");
+              new weeklyReport();
+              break;
+          
+          default:
+              System.out.println(x:"Invalid choice. Please try again.");
+      }
+  }
+
+public static void main (String[] args) {
+    Scanner scnr = new Scanner(System.in);
+
+    menu(scnr);
+    scnr.close();
+}
+
+
+
 
